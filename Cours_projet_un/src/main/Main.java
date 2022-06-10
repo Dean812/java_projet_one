f5178fe1b16ecc5ba447b36d59618f16a2ce828b
Thibaut <tibeloute@msn.com>
2022-06-10 08:47:59 +0200

    First commit Hello world

---
+++ Cours_projet_un/src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// cmd eclipse
+		// commenter/decommenter : CTRL + shilt + C
+		// indenter : CTRL + shilt + F
+
+		// Represente la methode principale, d'entree / sortie de notre programme
+		// Il ne peut y avoir qu'une methode [...] main(String[] args) dans une
+		// application
+
+		System.out.print("Hello print() !");
+		System.out.println("Hello ... !");
+		System.out.println("Hello println() !");
+
+		String hello = "Hello";
+		System.out.println(hello);
+
+		int x = 5;
+		System.out.println(x);
+		Integer y = 6;
+		System.out.println(y);
+
+		System.out.println("------------------------------------------------------------------");
+
+	}
+
+}
